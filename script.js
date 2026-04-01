6a2149a272486395c8ad9bbbfd3250adc249eb8c
Mohammed Ali <90979724+M0hammed-Ali@users.noreply.github.com>
2026-04-01 22:09:30 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelectorAll(".card").forEach(card => {
+  card.style.opacity = 0;
+  setTimeout(() => {
+    card.style.transition = "1s";
+    card.style.opacity = 1;
+  }, 300);
+});
